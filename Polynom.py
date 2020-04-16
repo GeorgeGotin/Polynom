d17d4a24ec0ebdf64901110cb8157b2c72989bb3
GeorgeGotin <56473382+GeorgeGotin@users.noreply.github.com>
2020-04-16 22:53:13 +0300

    Add files via upload

--- Polynom.py
+++ Polynom.py
@@ -1,11 +1,37 @@
+import time
+
+def NOD(a,b):
+	r = a % b
+	while not(r == 0):
+		a = b
+		b = r
+		r = a % b
+		
+	return b
+
 class polynom():
 	def __init__(self,c=[]):
 		self.c = c
+		self.check()
+	def check(self):
+		res = self
+		while res[res.deg()]==0 and res.deg() > 0:
+			res.c.pop()
+		self = res
+		return res
+		
 	def __str__(self):
+		if self == 0:
+			return '0'
 		res=''
 		for i in range(len(self)):
-			res += (('+' if self[i] > 0 else '') +(str(self[i]) if self[i] != 1 else '') + ('x' if i != 0 else '') +('^'+str(i) if i > 1 else '')) if self[i] != 0 else ''
-		return res[1:]
+			if self[i] == 0:
+				continue
+			res += '' if i == 0 or self[i] < 0 else '+'
+			res += str(self[i]) if self[i] != 1 or i == 0 else ''
+			res += 'x' if i != 0 else ''
+			res += '^'+str(i) if i != 0 and i != 1 else '' 
+		return res
 	def deg(self):
 		return len(self)-1
 	
@@ -38,7 +64,7 @@ class polynom():
 			res = []
 			for i in range(max(len(self),len(other))):
 				res.append(self[i]+other[i])
-			return polynom(res)		
+			return polynom(res).check()	
 	def __radd__(self,other):
 		return self+other
 	
@@ -51,7 +77,7 @@ class polynom():
 				res.append(0)
 				for i in range(k+1):
 					res[k]+=self[i]*other[k-i]
-			return polynom(res)	
+			return polynom(res).check()
 	def __rmul__(self,other):
 		return self*other
 	def __pow__(self,p):
@@ -61,44 +87,49 @@ class polynom():
 		return res
 	
 	def __sub__(self,other):
-		return self+other*(-1)	
+		return self+other*(-1)
 	def __rsub__(self,other):
 		return (-1)*self+other
 	def __neg__(self):
 		return self*(-1)
-		
-	def __floordiv__(self,other):
+	def __divmod__(self,other):		
 		if type(other) != polynom:
 			return self*(1/other)
 		else:
 			if other.deg() > self.deg():
-				return polynom([1])
+				res,mod = [0],self.check()
 			else:
 				mod = polynom(self.c.copy())
 				res = [0 for i in range(len(self)-len(other)+1)]
 				for step in range(len(self) - len(other),-1,-1):
 					res[step] = mod[other.deg()+step] / other[other.deg()]
 					mod = mod - other*res[step]*polynom([0 if i != step else 1 for i in range(step+1)])
-			
-				return polynom(res)
+		return polynom(res).check(),mod.check()
+	def __truediv__(self,other):
+		return self.__divmod__(other)[0]
 	def __mod__(self,other):
-		if type(other) != polynom:
-			return 0
-		else:
-			if other.deg() > self.deg():
-				return polynom([1])
-			else:
-				mod = polynom(self.c.copy())
-				res = [0 for i in range(len(self)-len(other)+1)]
-				for step in range(len(self) - len(other),-1,-1):
-					res[step] = mod[other.deg()+step] / other[other.deg()]
-					mod = mod - other*res[step]*polynom([0 if i != step else 1 for i in range(step+1)])
-			
-				return mod
+		return self.__divmod__(other)[1]
 	
-a = polynom([0,6,4,8])
-b = polynom([4,2])
-print(a,a+b,a*b)
-for i in a:
-	print(i,a.counter)
-print(a.counter)
+	def derivative(self):
+		res = []
+		for i in range(1,len(self)):
+			res.append(self[i]*i)
+		return polynom(res).check()
+	def del_smth(self):
+		hf = polynom(self.c.copy())
+		hd = hf.derivative()
+		return hf/NOD(hf,hd)
+	
+	def __eq__(self,other):
+		h = other
+		if type(h) != polynom:
+			h = polynom([h])
+		if len(self) != len(h):
+			return False
+		for i in range(len(self)):
+			if self[i] != h[i]:
+				return False
+		return True
+	
+a = polynom([1,0,-2,0,1])
+print(a.del_smth())
