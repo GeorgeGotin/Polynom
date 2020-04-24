d618eb5503457aaa6c7e02247d55587f92e45a71
GeorgeGotin <56473382+GeorgeGotin@users.noreply.github.com>
2020-04-25 02:10:42 +0300

    Add files via upload

--- Polynom.py
+++ Polynom.py
@@ -1,4 +1,5 @@
 import time
+import matplotlib.pyplot as plt
 
 def NOD(a,b):
 	r = a % b
@@ -8,6 +9,144 @@ def NOD(a,b):
 		r = a % b
 		
 	return b
+
+def power(a,n):
+	m = []
+	while n > 0:
+		m.append(n%2)
+		n = n // 2
+	s = 1
+	q = len(m)
+	for i in range(q):
+		if m[q - i - 1] == 0:
+			s = s*s
+		else:
+			s=s*s*a
+	return s
+	
+class frac:
+	def __init__(self,up,down=1):
+		if down == 0:
+			print('blue screen of  death(we can\'t div 0)')
+			return None
+		hup = float(up).as_integer_ratio()[0] * float(down).as_integer_ratio()[1]
+		hdown = float(up).as_integer_ratio()[1] * float(down).as_integer_ratio()[0]
+		c = NOD(int(hup),int(hdown))
+		self.up = hup // c
+		self.down = hdown // c
+		
+		
+	def __str__(self):
+		if self.down == 1:
+			return '{}'.format(self.up)
+		else:
+			return ('{}/{}'.format(self.up,self.down))
+			
+			
+	def __add__(self,other):
+		if type(other) == frac:
+			return frac(self.up*other.down+self.down*other.up,self.down*other.down)
+		else:
+			return self+frac(other)
+			
+	def __radd__(self,other):
+		return self + other
+			
+	
+	def __sub__(self,other):
+		if type(other) == frac:
+			return self + frac(-1*other.up,other.down)
+		else:
+			return self + frac(-1*other)
+			
+	def __rsub__(self,other):
+		return -self + other		
+			
+			
+	def __mul__(self,other):
+		if type(other) == frac:
+			return frac(self.up*other.up,self.down*other.down)
+		else:
+			return self*frac(other)
+			
+	def __rmul__(self,other):
+		return self * other
+		
+			
+	def __neg__(self):
+		return frac(-self.up,self.down)
+		
+		
+	def __truediv__(self,other):
+		if type(other) == frac:
+			return frac(self.up*other.down,self.down*other.up)
+		else:
+			return self / frac(other)
+			
+	def __rtruediv__(self,other):
+		return frac(self.down,self.up)*frac(other)
+			
+			
+	def __pow__(self,p):
+		return frac(power(self.up,p),power(self.down,p))
+	
+	
+	def __eq__(self,other):
+		if type(other) == frac:
+			if self.up == other.up and self.down == other.down:
+				return True
+			else:
+				return False
+		else:
+			return self == frac(other)
+	
+	def __ne__(self,other):
+		return not(self == other)
+	
+	def __lt__(self,other):
+		if type(other) == frac:
+			if (self - other).up < 0:
+				return True
+			else:
+				return False
+		else:
+			return self<frac(other)
+	
+	def __gt__(self,other):
+		if type(other) == frac:
+			if (self - other).up > 0:
+				return True
+			else:
+				return False
+		else:
+			return self>frac(other)
+	
+	def __le__(self,other):
+		if type(other) == frac:
+			if (self - other).up <= 0:
+				return True
+			else:
+				return False
+		else:
+			return self<=frac(other)
+			
+	def __ge__(self,other):
+		if type(other) == frac:
+			if (self - other).up >= 0:
+				return True
+			else:
+				return False
+		else:
+			return self>=frac(other)
+			
+		
+	def __float__(self):
+		return self.up/self.down
+		
+	def __abs__(self):
+		return frac(abs(self.up),abs(self.down))
+
+
 
 class polynom():
 	def __init__(self,c=[]):
@@ -28,7 +167,7 @@ class polynom():
 			if self[i] == 0:
 				continue
 			res += '-' if self[i] < 0 else '' if i == 0 else '+'
-			res += str(abs(self[i])) if abs(self[i]) != 1 or i == 0 else ''
+			res += str(abs(self[i] if not self[i].is_integer() else int(self[i])) ) if abs(self[i]) != 1 or i == 0 else ''
 			res += 'x' if i != 0 else ''
 			res += '^'+str(i) if i != 0 and i != 1 else '' 
 		return res
@@ -51,8 +190,8 @@ class polynom():
 			return self.c[i] if i <= self.deg() else 0
 		else:
 			res = []
-			for j in range(i.start,i.stop,(i.step if i.step != None else 1)):
-				res.append(j)
+			for j in range((i.start if i.start != None else 0),(i.stop if i.stop != None else len(self)),(i.step if i.step != None else 1)):
+				res.append(self[j])
 			return res
 	def __len__(self):
 		return len(self.c)
@@ -130,6 +269,99 @@ class polynom():
 			if self[i] != h[i]:
 				return False
 		return True
+		
+	def __call__(self,x):
+		res = 0
+		h = 1
+		for i in self:
+			res += h*i
+			h*=x
+		return res
 	
-a = polynom([1,0,-2,0,1])
-print(a.del_smth())
+	def interval_with_all_roots(self):
+		return 1+(max([abs(i) for i in self[1:]])/abs(self[self.deg()]))
+	
+	def Sturm_system(self):
+		res = [self,self.derivative()]
+		i = 1
+		while res[-1] != 0:
+			res.append(res[i-1]%res[i] * (-1))
+			i+=1
+		return res[:-1]
+
+def W(Sturm_system,c):
+	h = []
+	for i in Sturm_system:
+		if i(c) == 0:
+			continue
+		else:
+			h.append(i(c))
+	res = 0
+	for i in range(1,len(h)):
+		if (h[i] > 0 and h[i-1] < 0) or (h[i] < 0 and h[i-1] > 0):
+			res+=1
+	return res
+
+def get_intervals_with_roots(f):
+	'''
+	Возвращает список интервалов, содержащих по одному корню
+	многочлена [(a0, b0), (a1, b1), ..., (am, bm)]
+	'''
+	# 1й шаг: строим систему Штурма
+	# 2й шаг: находим K
+	# 3й шаг: находим интервалы половинным делением, используя функцию W
+	syst = f.Sturm_system()
+	k = f.interval_with_all_roots()
+	intrs = [-k,k]
+	WK = W(syst,-k)-W(syst,k)
+	t = 1
+	tail=0
+	while len(intrs)-1 < WK or t > 0:
+		t-=1
+		for i in range(1,len(intrs)-tail):
+			if W(syst,intrs[i-1]) - W(syst,intrs[i]) > 1:
+				intrs.append((intrs[i-1]+intrs[i])/2)
+				intrs.sort()
+				t += 1
+			elif W(syst,intrs[i-1]) - W(syst,intrs[i]) == 0:
+				intrs[i-1] = k+1
+				tail +=1
+	return intrs[:-tail] if tail != 0 else intrs
+
+def calculate_root_on_interval(f, a, b):
+	'''
+	Возвращает корень x0 на интервале (a, b)
+	'''
+	# Находим корень половинным делением, выбирая половину,
+	# на которой различаются знаки
+	a = frac(a)
+	b = frac(b)
+	while abs(b-a)>0.0001:
+		c = (a+b)/2
+		if f(c) == 0:
+			return float(c) if f(round(float(c))) != 0 else round(float(c))
+		if f(a)*f(c) < 0:
+			b = c
+		else:
+			a = c
+	return float((a+b)/2) if f(round(float((a+b)/2))) != 0 else round(float((a+b)/2))
+
+def get_all_roots(f):
+	'''
+	Возвращает список корней [x1, x2, ..., xm]
+	'''
+	res =[]
+	intrs = get_intervals_with_roots(f)
+	for i in range(len(intrs)-1):
+		if f(intrs[i]) == 0:
+			res.append(intrs[i])
+		elif f(intrs[i+1]) == 0:
+			res.append(intrs[i+1])
+		res.append(calculate_root_on_interval(f,intrs[i],intrs[i+1]))
+	res = list(set(res))
+	res.sort()
+	return res
+
+a = polynom([0,-6,-1,1])
+print(0.0)
+print(get_all_roots(a))
